8a7591d2f3cfd9a5b3a198cefa34bdb68eab959e
Iskender882 <isken.tabyldiev@icloud.com>
2021-05-30 17:17:19 +0300

    добавил Switch  и изменил код

--- src/com/geektech/Main.java
+++ src/com/geektech/Main.java
@@ -2,24 +2,37 @@ package com.geektech;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-        String mame = "Isken";
-        int age = 14;
-        int temp = 25;
-        if (age < 20 && age < 45 && temp < +30 && temp  > -20
-         && temp > 0 && temp < 28 ) {
-            System.out.println("Можно идти гулять");
-            if (age < 20 && temp > 0 && temp < 28){
-                System.out.println("Можно гулять");
-                if (age > 45 || temp > -10 && temp < 25){
-                    System.out.println(" То Можно гулять");
+        String name = ("Isken");
 
-                }
-            }
+        switch (name) {
+            case "Alex":
+                System.out.println("Его имя Alex");
+                break;
+            case "Steve":
+                System.out.println("Его имя Steve");
+                break;
+            case "Isken":
+                System.out.println("Его имя Isken");
+                break;
+            default:
+                System.out.println("Неизвестно");
         }
 
+        int temp = 25;
+        int age = 14;
+        if (age < 20 && age > 45 && temp < +30 && temp > -20
+                && temp > 0 && temp < 28) {
+            System.out.println("Можно идти гулять");
+        }else if  (age < 20 && temp > 0 && temp < 28) {
+            System.out.println("Можно гулять");
+            if (age < 45 || temp > -10 && temp < 25){
+                System.out.println("То можно гулять");
+            }
+
+
+        }
     }
 
 }
